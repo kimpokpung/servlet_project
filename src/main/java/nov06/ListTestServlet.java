cbfe7892482b2ab2d539b2ef81aec18d3efb4212
kimpokpung <gh8897@naver.com>
2023-12-10 17:28:33 +0900

    test8

--- src/main/java/nov06/ListTestServlet.java
+++ src/main/java/nov06/ListTestServlet.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServle;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
